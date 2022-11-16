67f3fb6b80d346618c463fd0728cdbed5bb2f8b0
VP <vpishchanyuk@gmail.com>
2022-11-16 14:45:24 +0300

    data

--- src/main.py
+++ src/main.py
@@ -2,3 +2,8 @@ import numpy as np
 import pandas as pd
 from scipy import sparce
 from sklearn.preprocessing import normalize
+
+interactions_df = pd.read_csv("data/lastfm_user_scrobbles.csv")
+titles_df = pd.read_csv("data/lastfm_artist_list.csv")
+
+interactions_df
