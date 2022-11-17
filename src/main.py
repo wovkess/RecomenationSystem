df77be25a49a9e7a0890b738c81a9a4a4133a6a2
VP <vpishchanyuk@gmail.com>
2022-11-17 17:21:13 +0300

    jupyter_start

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
-from scipy import sparce
+from scipy import sparse
 from sklearn.preprocessing import normalize
 
-interactions_df = pd.read_csv("data/lastfm_user_scrobbles.csv")
-titles_df = pd.read_csv("data/lastfm_artist_list.csv")
-
+interactions_df = pd.read_csv(
+    "D:/Study/Python/RecomenationSystem/data/lastfm_user_scrobbles.csv")
+titles_df = pd.read_csv(
+    "D:/Study/Python/RecomenationSystem/data/lastfm_artist_list.csv")
 interactions_df
