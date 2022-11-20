220d27ecef85da840c138cc5ded1e69e9b4f355c
VP <vpishchanyuk@gmail.com>
2022-11-20 16:20:04 +0300

    get_rec

--- src/recomendations.py
+++ src/recomendations.py
@@ -1,12 +1,14 @@
 from math import sqrt
+import numpy
+# critics dictionary
 critics = {
     'Lisa Rose': {
-        'Lady of the water': 2.5,
+        'Lady in the water': 2.5,
         'Snakes on a Plane': 3.5,
-        'Just my luck': 3.0,
+        'Just My Luck': 3.0,
         'Superman Returns': 3.5,
         'You, Me and Dupree': 2.5,
-        'The Wight Listener': 3.0
+        'The Night Listener': 3.0
     },
     'Gene Seymour': {
         'Lady in the Water': 3.0,
@@ -29,12 +31,14 @@ critics = {
         'Superman Returns': 4.0,
         'You, Me and Dupree': 2.5,
     },
-    'Person1': {
+    'Vova': {
         'Snakes on a Plane': 4.5,
         'You, Me and Dupree': 1.0,
         'Superman Returns': 4.0
     }
 }
+
+# Evklid distance
 
 
 def sim_distance(prefs, person1, person2):
@@ -50,33 +54,70 @@ def sim_distance(prefs, person1, person2):
     return 1 / (1 + sqrt(sum_of_squares))
 
 
-def sim_person(prefs, person1, person2):
+# koeff korr Pirsona
+
+
+def sim_pearson(prefs, person1, person2):
     si = {}
     for item in prefs[person1]:
         if item in prefs[person2]:
             si[item] = 1
+    if len(si) == 0:
+        return 0
 
-        if len(si) == 0:
-            return 0
+    sum1 = sum([prefs[person1][item] for item in si])
+    sum2 = sum([prefs[person2][item] for item in si])
 
-        sum1 = sum([prefs[person1][item] for item in si])
-        sum2 = sum([prefs[person2][item] for item in si])
+    sum1_sq = sum([pow(prefs[person1][item], 2) for item in si])
+    sum2_sq = sum([pow(prefs[person2][item], 2) for item in si])
 
-        sum1_sq = sum([pow(prefs[person1][item], 2) for item in si])
-        sum2_sq = sum([pow(prefs[person2][item], 2) for item in si])
+    p_sum = sum([prefs[person1][item]*prefs[person2][item] for item in si])
 
-        p_sum = sum([prefs[person1][item]*prefs[person2][item] for item in si])
+    num = p_sum - (sum1 * sum2/len(si))
+    den = sqrt((sum1_sq - pow(sum1, 2)/len(si)) *
+               (sum2_sq - pow(sum2, 2) / len(si)))
 
-        num = p_sum - (sum1 * sum2/len(si))
-        den = sqrt((sum1_sq - pow(sum1, 2)/len(si)) *
-                   (sum2_sq - pow(sum2, 2) / len(si)))
+    if den == 0:
+        return 0
 
-        if den == 0:
-            return 0
+    return num/den
 
-        return num/den
+# top films
 
 
-if __name__ == '__main__':
-    print(sim_distance(critics, "Person1", "Lisa Rose"))
-    print(sim_person(critics, "Person1", "Lisa Rose"))
+def top_matches(prefs, person, n=5, simularity=sim_pearson):
+    scores = [(simularity(prefs, person, other), other)
+              for other in prefs if other != person]
+
+    scores.sort()
+    scores.reverse()
+    return scores[0:n]
+
+# top recommendations
+
+
+def get_rec(prefs, person, simularity=sim_pearson):
+    totals = {}
+    sim_sums = {}
+    for other in prefs:
+        if other == person:
+            continue
+        sim = simularity(prefs, person, other)
+        if sim <= 0:
+            continue
+        for item in prefs[other]:
+            if item not in prefs[person] or prefs[person][item] == 0:
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+
+                sim_sums.setdefault(item, 0)
+                sim_sums[item] += sim
+    rank = [(total/sim_sums[item], item) for item, total in totals.items()]
+
+    rank.sort()
+    rank.reverse()
+    return rank
+
+
+if __name__ == "__main__":
+    print(get_rec(critics, "Vova"))
